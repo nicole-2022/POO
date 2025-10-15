600dd406fcb2a102cef89c8ad6d0ad2776944ae0
nicole-2022 <nicole.sanchezc@campusucc.edu.co>
2025-10-15 02:23:09 +0000

    AVANCE

---
+++ expresines.java
@@ -0,0 +1,11 @@
+public class expresines {}
+ public static void main (string [] arges) {}
+
+   /*
+   *evuluar la siguiente expresión para A=2 y B = 5; 3*A - 4* B /A^ 2
+   */
+
+ int a = 2; 
+ int b = 5;
+
+ siystem.aut.println(3* a - 4 * b / math.pow(a,b,: 2))
